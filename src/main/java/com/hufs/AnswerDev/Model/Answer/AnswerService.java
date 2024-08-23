dfd113506cd8650e566d01d0f371d01d103079cc
kimdevv <kim__dev@hufs.ac.kr>
2024-08-24 00:02:37 +0900

    Feat. 셀레니움 설정

--- src/main/java/com/hufs/AnswerDev/Model/Answer/AnswerService.java
+++ src/main/java/com/hufs/AnswerDev/Model/Answer/AnswerService.java
@@ -33,7 +33,7 @@ public class AnswerService {
         String url = dto.getUrl();
         Answer existAnswer = answerRepository.findByUrl(url);
 
-        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("window-size=1400,1500");
