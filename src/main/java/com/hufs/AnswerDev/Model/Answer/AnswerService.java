cb3a04aa7b94c389da17253a14fbeee1322a0af3
kimdevv <kim__dev@hufs.ac.kr>
2024-08-25 22:44:40 +0900

    Refactor. 드라이버 세팅

--- src/main/java/com/hufs/AnswerDev/Model/Answer/AnswerService.java
+++ src/main/java/com/hufs/AnswerDev/Model/Answer/AnswerService.java
@@ -33,12 +33,12 @@ public class AnswerService {
         String url = dto.getUrl();
         Answer existAnswer = answerRepository.findByUrl(url);
 
-        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+//        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-gpu");
-        options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-blink-features=AutomationControlled");
+//        options.addArguments("--disable-gpu");
+//        options.addArguments("--headless");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--disable-blink-features=AutomationControlled");
         WebDriver driver = new ChromeDriver(options);
         try {
             driver.get(url);
